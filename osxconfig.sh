62870a4860c56260221499f919e7fde336182548
Julius Huelsmann <huelsmann@campus.tu-berlin.de>
2017-04-24 00:20:21 +0200

    reorganization fo scripts

--- osxconfig.sh
+++ osxconfig.sh
@@ -2,6 +2,6 @@
 #
 # This file contains the defintion of the location 
 # of the cofiguration source and destination.
-locQute="$HOME/Library/Preferences/qutebrowser/"
-conQute="qutebrowser.conf"
+locQute="$HOME/Library/Preferences/qutebrowser/qutebrowser.conf"
+conQute="config/qutebrowser.conf"
 
