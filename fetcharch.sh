81290420b6f00225fc9dc823e4b62745803efb3f
Julius Huelsmann <huelsmann@campus.tu-berlin.de>
2017-04-23 23:44:34 +0200

    refactor and modification

---
+++ fetcharch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Include the x-copy script that can be used for 
+# copying only in case the edit-timestamp of the
+# file to be overwritten is newer.
+source cpUtils.sh
+
+
+#
+# Defintion of the location of the cofiguration
+# source and destination.
+source archconfig.sh
+
+
+#
+# Execute the actual copy command.
+xcp $locQuote $conQute
+
+
+
+
