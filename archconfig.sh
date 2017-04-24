25d294fc292d7ea6ac1a530e8278f597a18f1b9f
Julius Huelsmann <huelsmann@campus.tu-berlin.de>
2017-04-24 22:33:12 +0200

    .

--- archconfig.sh
+++ archconfig.sh
@@ -2,5 +2,5 @@
 #
 # This file contains the defintion of the location 
 # of the cofiguration source and destination.
-locQute="$HOME/.config/qutebrowser/"
-conQute="qutebrowser.conf"
+locQute="$HOME/.config/qutebrowser/qutebrowser.conf"
+conQute="config/qutebrowser.conf"
