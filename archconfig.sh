49c7e7f6cd034b54f46e67562e3aa2d01b7d2fbe
julius huelsmann <huelsmann@campus.tu-berlin.de>
2017-09-06 21:52:44 +0200

    removed config files

--- archconfig.sh
+++
@@ -1,6 +0,0 @@
-
-#
-# This file contains the defintion of the location 
-# of the cofiguration source and destination.
-locQute="$HOME/.config/qutebrowser/"
-conQute="configs/qutebrowser.conf"
--- osxconfig.sh
+++
@@ -1,7 +0,0 @@
-
-#
-# This file contains the defintion of the location 
-# of the cofiguration source and destination.
-locQute="$HOME/Library/Preferences/qutebrowser/"
-conQute="configs/qutebrowser.conf"
-
