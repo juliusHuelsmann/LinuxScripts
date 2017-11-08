ff4621bd8a7b13282e61a604753a6c1ca6489ccf
Julius Huelsmann <huelsmann@campus.tu-berlin.de>
2017-11-08 20:08:50 +0100

    auto-commit: security pre-remove commit (README.md). If this commit is staged, sb forgot to commit!

--- pushHook.sh
+++ pushHook.sh
@@ -2,8 +2,9 @@
 # fail if you fail 
 
 # security commit
+# in case sb forgot to commit prior to pushing
 echo -e        "auto-commit: security pre-remove commit (README.md)"
-git commit -am "auto-commit: security pre-remove commit (README.md)"
+git commit -am "auto-commit: security pre-remove commit (README.md). If this commit is staged, sb forgot to commit!"
 
 # generate README.md
 rm -f README.md
